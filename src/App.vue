a5e154226b20e1b10ff747776b7593da2ffd41b9
usuarioJapa <willian.hsimabukuro@gmail.com>
2018-07-27 17:35:23 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <header>
+      <img src="./assets/logo.png">
+      <nav>
+        <ul>
+          <li><router-link :to="{ name: 'Create' }">Create</router-link></li>
+          <li><router-link :to="{ name: 'Preview' }">Preview</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+      <p>made by <span>userJapa</span></p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import 'assets/sass/main.scss';
+
+#app {
+  height: 100%;
+  display: grid;
+  grid-template-rows: 1fr 4fr .5fr;
+  header {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    img, nav {
+      align-self: center;
+      display: inline-block;
+    }
+    img {
+      width: 12.5%;
+      height: auto;
+    }
+    nav {
+      text-align: center;
+      ul {
+        list-style: none;
+        li {
+          font-size: 2rem;
+          display: inline;
+          a {
+            text-decoration: none;
+            color: $color-black;
+            &:first-child { margin-left: 10px; }
+          }
+        }
+      }
+    }
+  }
+  footer {
+    font-size: 2rem;
+    display: flex;
+    justify-content: flex-end;
+    p {
+      align-self: flex-end;
+      display: inline;
+    }
+  }
+}
+
+</style>
---
+++ src/pages/Create.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div class="create">
+    <h5>Create</h5>
+    <div class="create__box">
+      <div class="create__box__game">
+        <ImageSpeech/>
+      </div>
+      <div class="create__box__form">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageSpeech from '@/components/ImageSpeech'
+
+export default {
+  name: 'Create',
+  components: {
+    ImageSpeech
+  }
+}
+</script>
+
+<style lang="scss">
+  .create {
+    height: 100%;
+    font-size: 3rem;
+    h5 {
+      text-align: center;
+    }
+    &__box {
+      height: 100%;
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+    }
+  }
+</style>
